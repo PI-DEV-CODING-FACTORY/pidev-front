b8686fca74c5509df2462287a5815b41a941687c
Yassine-Essid <yassinessid10@gmail.com>
2025-04-17 10:43:13 +0100

    the inscriptions component routing WIP

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
             {
                 path: 'users',
                 loadComponent: () => import('./components/users-list/users-list.component').then(m => m.UsersListComponent)
+            },
+            {
+                path: 'inscriptions',
+                loadComponent: () => import('./components/inscriptions/inscriptions.component').then(m => m.InscriptionsComponent)
             }
         ]
     }
